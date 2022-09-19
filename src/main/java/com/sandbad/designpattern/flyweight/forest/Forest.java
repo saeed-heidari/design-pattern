e5abb438195564da0b542b02b5755f5fc94300e5
heidari <heidari@sandbadcell.sandbadcell>
2022-09-19 15:14:45 +0430

    flyweight

---
+++ src/main/java/com/sandbad/designpattern/flyweight/forest/Forest.java
@@ -0,0 +1,29 @@
+package com.sandbad.designpattern.flyweight.forest;
+
+import com.sandbad.designpattern.flyweight.trees.Tree;
+import com.sandbad.designpattern.flyweight.trees.TreeFactory;
+import com.sandbad.designpattern.flyweight.trees.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+
+}
---
+++ src/main/java/com/sandbad/designpattern/flyweight/trees/Tree.java
@@ -0,0 +1,19 @@
+package com.sandbad.designpattern.flyweight.trees;
+
+import java.awt.*;
+
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics graphics) {
+        type.draw(graphics, x, y);
+    }
+}
