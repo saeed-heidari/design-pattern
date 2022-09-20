b6c813f678ead9a9a102ad6828946b9fb9b8e511
heidari <heidari@sandbadcell.sandbadcell>
2022-09-20 12:22:19 +0430

    command

---
+++ src/main/java/com/sandbad/designpattern/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package com.sandbad.designpattern.commands;
+
+import com.sandbad.designpattern.commands.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
