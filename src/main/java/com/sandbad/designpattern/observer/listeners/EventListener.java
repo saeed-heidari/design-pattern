8add05441b6772e9fbeff088e98fb73212ad1717
root <root@sandbadcell.sandbadcell>
2022-10-02 17:42:58 +0330

    STRATEGY

---
+++ src/main/java/com/sandbad/designpattern/observer/listeners/EventListener.java
@@ -0,0 +1,7 @@
+package com.sandbad.designpattern.observer.listeners;
+
+import java.io.File;
+
+public interface EventListener {
+    void update(String eventType, File file);
+}
---
+++ src/main/java/com/sandbad/designpattern/state/states/State.java
@@ -0,0 +1,20 @@
+package com.sandbad.designpattern.state.states;
+
+import com.sandbad.designpattern.state.ui.Player;
+
+public abstract class State {
+    Player player;
+
+    /**
+     * Context passes itself through the state constructor. This may help a
+     * state to fetch some useful context data if needed.
+     */
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
